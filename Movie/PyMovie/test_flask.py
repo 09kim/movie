375b4575420959f3a2764f0bf0abfe38f668bb4e
fmoni1306 <69664189+fmoni1306@users.noreply.github.com>
2020-09-14 22:07:27 +0900

    2020-09-14 네이버 검색어 크롤링 한거 제이슨으로 가져오기

--- Movie/PyMovie/test_flask.py
+++ Movie/PyMovie/test_flask.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import recommend, sql
+import recommend, sql,getNaver
 
 app = Flask(__name__)
 
@@ -9,19 +9,23 @@ li = []
 
 
 # @app.route('/recommendUser.mo', methods=['post'])
-@app.route('/recommendUser.mo/<nick>')
+@app.route('/recommendUser/<nick>')
 def recommendUser(nick):
     member, lst = sql.getMember()
     li = recommend.top_match(member, nick)
     return jsonify(li)
 
 
-@app.route('/expectedGrade.mo/<nick>')
+@app.route('/expectedGrade/<nick>')
 def expectedGrade(nick):
     member, lst = sql.getMember()
     li = recommend.getRecommendation(member, nick)
     return jsonify(li)
 
+@app.route('/getNaverRanking/')
+def getNaverRanking():
+    li = getNaver.getNaver();
+    return jsonify(li)
 
 if __name__ == "__main__":
     app.run()
